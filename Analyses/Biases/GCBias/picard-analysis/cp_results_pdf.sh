6a19b8f1f99a1fd64f7c72759634dfade8ae619b
Michael Bradshaw <michaelscottbradshaw@gmail.com>
2020-04-15 15:24:40 -0600

    cleaned up gcbias analysis dirrectory

--- Analyses/Biases/GCBias/picard-analysis/cp_results_pdf.sh
+++
@@ -1,9 +0,0 @@
-cd results
-for d in *
-do
-	file="$d/gc_bias_metrics.pdf"
-	echo $file
-	newfile="/Users/mibr6115/GCBiasResults/$d"
-	extension="_gc_bias_metrics.pdf"
-	cp $file "$newfile$extension"
-done
