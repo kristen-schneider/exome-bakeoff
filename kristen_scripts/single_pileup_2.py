4bb3ff07cf41bc3c2ca0b614ea43c1b3de996bdb
Kristen Schneider <krsc0813@fiji-1.colorado.edu>
2020-04-22 11:07:54 -0600

    cleanup unused code

--- kristen_scripts/single_pileup_2.py
+++ kristen_scripts/single_pileup_2.py
@@ -60,37 +60,3 @@ def read_regions(tbx_pileup_file, regions_path, metrics_options, sample_name):
 
 
 
-
-    # regions_file_read = open(regions_file, 'r')
-    #
-    # for region in regions_file_read:
-    #     region = region.rstrip().split()
-    #     # convert chrm, start, end to proper type
-    #     try: region_chrm = int(region[0])
-    #     except ValueError: region_chrm = region[0]
-    #     region_start = int(region[1])
-    #     region_end = int(region[2])
-    #
-    #     # fetch regions
-    #     for row in tbx_pileup_file.fetch(region_chrm, region_start, region_end, parser=pysam.asBed()):
-    #         # get necessary values
-    #         q_chrm = row[0]
-    #         q_start = row[1]
-    #         q_end = row[2]
-    #         if "quality" in metrics_options:
-    #             quality = main_quality(row)
-    #             # format: chrm, start, end, quality_score
-    #             quality_output_txt.write(q_chrm + '\t' + q_start + '\t' + q_end  + '\t' + str(quality) + '\t' + region[4] + '\n')
-    #
-    #         if "strandbias" in metrics_options:
-    #             strandbias = 0
-    #             # call strandbias script here
-    #
-    #         if "noise" in metrics_options:
-    #             noise = 0
-    #             # call noise script here
-    #
-    #         # ... and so on
-
-
-
