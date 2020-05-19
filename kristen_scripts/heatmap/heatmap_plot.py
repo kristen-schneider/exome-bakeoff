e7bdb028d2fe24816a579e30d800d1de8bcf7357
Kristen Schneider <krsc0813@fiji-1.colorado.edu>
2020-05-19 10:08:58 -0600

    heatmap plot takes cmdln args

--- kristen_scripts/heatmap/heatmap_plot.py
+++ kristen_scripts/heatmap/heatmap_plot.py
@@ -20,7 +20,7 @@ def main_heatmap():
     all_sample_metrcis = []
 
     for sample in os.listdir(heatmap_metrics_path):
-        if "-hm" in sample:
+        if "_hm" in sample:
             # store sample names
             curr_sample_name = sample.split('-')
             curr_sample_name = curr_sample_name[0]+'-'+curr_sample_name[1]+'-'+curr_sample_name[2]
@@ -35,19 +35,18 @@ def main_heatmap():
                 metric = line [1]
                 if gene not in gene_names: gene_names.append(gene)
                 curr_sample_gene_data[gene] = metric
-
             # sort sample metrics by gene name
             genes_sorted = sorted(curr_sample_gene_data)
-            print(genes_sorted)
+            #print(genes_sorted)
 
             metrics_sorted_by_gene = []
             for gene in genes_sorted: metrics_sorted_by_gene.append(float(curr_sample_gene_data[gene]))
 
             all_sample_metrcis.append(metrics_sorted_by_gene)
 
-    plot_heatmap(sample_names, gene_names, all_sample_metrcis, TITLE)
+    plot_heatmap(sample_names, gene_names, all_sample_metrcis, TITLE, heatmaps_figure_path)
 
-def plot_heatmap(sample_names, gene_names, all_sample_metrcis, title):
+def plot_heatmap(sample_names, gene_names, all_sample_metrcis, title, heatmaps_figure_path):
     fig, ax = plt.subplots()
     fig.set_size_inches(35.0, 25.0)
     ax.set_xticks(np.arange(len(gene_names)))
