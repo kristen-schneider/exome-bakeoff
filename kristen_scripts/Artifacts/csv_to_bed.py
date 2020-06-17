173606c71e779432eb4864d2a3c79f65d76ea35d
Kristen Schneider <krsc0813@fiji-1.colorado.edu>
2020-06-17 14:39:38 -0600

    trying to see if Rebeccas's sites are hq for us as well

---
+++ kristen_scripts/Artifacts/csv_to_bed.py
@@ -0,0 +1,18 @@
+import os
+
+artifact_csv = open('/Users/krsc0813/exome-bakeoff/kristen_scripts/Artifacts/HighQual.csv', 'r')
+
+def main():
+    artifact_bed = open('/Users/krsc0813/exome-bakeoff/kristen_scripts/Artifacts/HighQual.bed', 'a')
+    artifact_bed.truncate(0)
+    header = None
+    
+    for line in artifact_csv:
+        if header == None: header = line
+        else:
+            A = line.rstrip().split(',')
+            artifact_bed.write(A[0] + '\t' + A[1] + '\t' + str(float(A[1])+1.) + '\n')
+    
+        
+
+main()
